8c5d547fa58c708cfdfb0ea16ae3c20fcbf029c1
arikkfir <arik@kfirs.com>
2025-10-03 00:28:14 +0300

    fix(infra): grant subscriber role to Pub/Sub GCP service account
    This is required for it to be able to manage dead-lettering in topics.

--- infra/pubsub.tf
+++ infra/pubsub.tf
@@ -1,5 +1,9 @@
 resource "google_project_iam_member" "gcp_pubsub_publish" {
+  for_each = toset([
+    "roles/pubsub.publisher",
+    "roles/pubsub.subscriber",
+  ])
   project = var.project_id
-  role    = "roles/pubsub.publisher"
+  role    = each.key
   member  = "serviceAccount:service-${data.google_project.current.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
 }
