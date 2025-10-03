297078eb99b615dc73444e51360ed337d5206893
arikkfir <arik@kfirs.com>
2025-10-03 17:12:05 +0300

    perf(dispatcher): increase timeout for the dispatcher job

--- infra/dispatcher.tf
+++ infra/dispatcher.tf
@@ -45,7 +45,7 @@ resource "google_cloud_run_v2_job" "dispatcher" {
   template {
     template {
       service_account = google_service_account.dispatcher.email
-      timeout         = "${60 * 60 * 4}s"
+      timeout         = "${60 * 60 * 24 * 6}s"
       max_retries     = 3
       containers {
         image = "${google_artifact_registry_repository.ghcr_proxy.registry_uri}/arikkfir-org/gmail-organizer/dispatcher:${var.image_tag}"
