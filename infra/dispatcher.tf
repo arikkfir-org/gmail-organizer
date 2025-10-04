f6969bcbfaa4dd8c5f2cd96116cfe34c2b895a86
arikkfir <arik@kfirs.com>
2025-10-04 11:31:17 +0300

    chore(job): disable cloud run job retries

--- infra/dispatcher.tf
+++ infra/dispatcher.tf
@@ -38,7 +38,7 @@ resource "google_cloud_run_v2_job" "dispatcher" {
     template {
       service_account = google_service_account.dispatcher.email
       timeout         = "${60 * 60 * 24 * 6}s"
-      max_retries     = 3
+      max_retries     = 1
       containers {
         image = "${google_artifact_registry_repository.ghcr_proxy.registry_uri}/arikkfir-org/gmail-organizer/dispatcher:${var.image_tag}"
         resources {
