3e47d8617fabb984b08703883a3b849e818910e0
arikkfir <arik@kfirs.com>
2025-10-04 11:34:03 +0300

    chore(dispatcher): increase termination grace period to cloud run job
    The default is 10sec, this change increases that to 30sec.

--- infra/dispatcher.tf
+++ infra/dispatcher.tf
@@ -37,9 +37,10 @@ resource "google_cloud_run_v2_job" "dispatcher" {
   launch_stage        = "BETA"
   template {
     template {
-      service_account = google_service_account.dispatcher.email
-      timeout         = "${60 * 60 * 24 * 6}s"
-      max_retries     = 1
+      service_account                  = google_service_account.dispatcher.email
+      timeout                          = "${60 * 60 * 24 * 6}s"
+      max_retries                      = 1
+      termination_grace_period_seconds = 30
       containers {
         image = "${google_artifact_registry_repository.ghcr_proxy.registry_uri}/arikkfir-org/gmail-organizer/dispatcher:${var.image_tag}"
         resources {
